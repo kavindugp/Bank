971370e6a71e7a0fc85cd9ffeb722c9aaccece62
Kavindu Geesara <38973598+kavindugp@users.noreply.github.com>
2019-05-05 21:39:06 +0530

    Add files via upload

---
+++ Bank/src/bank/Credit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author kavindu
+ */
+public class Credit extends Visa{
+    
+    public static void widthrow(){
+        
+        Visa ac1=new Visa();
+        ac1.checkBalance();
+    }
+    
+}
---
+++ Bank/src/bank/Visa.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bank;
+
+/**
+ *
+ * @author kavindu
+ */
+public class Visa extends CreditSaving{
+    
+    public static void checkBalance(){
+        
+        CreditSaving ac2=new CreditSaving();
+        ac2.accoutValidation();
+        
+    }
+    
+}
